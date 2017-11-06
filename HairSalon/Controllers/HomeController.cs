826a5242557dad765a548fb4b1c4aed77d3333fe
Marcus Parmentier <marcusjparmentier@gmail.com>
2017-11-05 21:07:39 -0800

    add to controller and views to be able to add clients

--- HairSalon/Controllers/HomeController.cs
+++ HairSalon/Controllers/HomeController.cs
@@ -17,21 +17,56 @@ namespace HairSalon.Controllers
     [HttpGet("/stylists")]
     public ActionResult Stylists()
     {
-        return View(Stylist.GetAll());
+      return View(Stylist.GetAll());
     }
 
     [HttpGet("/stylists/new")]
     public ActionResult StylistForm()
     {
-        return View();
+      return View();
     }
 
     [HttpPost("/stylists/new")]
     public ActionResult StylistCreate()
     {
-        Stylist newStylist = new Stylist(Request.Form["stylist-name"], Request.Form["stylist-details"]);
-        newStylist.Save();
-        return View("Success");
+      Stylist newStylist = new Stylist(Request.Form["stylist-employee"], Request.Form["stylist-details"]);
+      newStylist.Save();
+      return View("Success");
+    }
+
+    [HttpGet("/stylists/{id}")]
+    public ActionResult StylistClients(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Stylist selectedStylist = Stylist.Find(id);
+      List<Client> stylistClients = selectedStylist.GetClient();
+      model.Add("stylist", selectedStylist);
+      model.Add("clients", stylistClients);
+      return View(model);
+    }
+
+    [HttpPost("/stylists/{id}")]
+    public ActionResult StylistClients2(int id)
+    {
+      Client newClient = new Client((Request.Form["inputName"]),(Request.Form["inputNotes"]), id);
+      newClient.Save();
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Stylist selectedStylist = Stylist.Find(Int32.Parse(Request.Form["stylist-id"]));
+      List<Client> stylistClients = selectedStylist.GetClient();
+      model.Add("clients", stylistClients);
+      model.Add("stylist", selectedStylist);
+      return View("stylistClients", model);
+    }
+
+    [HttpGet("/stylists/{id}/clients/new")]
+    public ActionResult ClientForm(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Stylist selectedStylist = Stylist.Find(id);
+      List<Client> allClients = selectedStylist.GetClient();
+      model.Add("stylist", selectedStylist);
+      model.Add("clients", allClients);
+      return View(model);
     }
 
   }
